56fbf1806ac11cfd1694118a8646e705cae73d61
rydnr <github@acm-sl.org>
2024-10-28 18:58:50 +0100

    Updated nixpkgs

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
     "Infrastructure layer for pythoneda-sandbox-artifact/python-artifact";
   inputs = rec {
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
-    nixos.url = "github:NixOS/nixpkgs/23.11";
+    nixos.url = "github:NixOS/nixpkgs/24.05";
     pythoneda-shared-artifact-events-infrastructure = {
       inputs.flake-utils.follows = "flake-utils";
       inputs.nixos.follows = "nixos";
